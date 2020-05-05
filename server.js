f51b8fd2146d604f270713f22d8198cc184bec07
YaroslabAbramov <yaroslav.abramov@agilians.global>
2020-05-05 12:14:00 +0300

    add analytics support

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const axios = require('axios')
+const path = require('path');
+var internetAvailable = require('internet-available');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+// analytics
+let analyticsRequests = [];
+const analyticsRequestsQueue = Promise.resolve()
+
+app.get('/analytics', (req, res) => {
+  analyticsRequests.push({
+    userAgent: req.headers['user-agent'],
+    query: req._parsedOriginalUrl.query
+  });
+  return res.send('ok');
+ });
+
+const sendAnalyticsRequest = ({ query, userAgent }) => {
+  return axios.post(
+    'https://www.google-analytics.com/debug/collect',
+    query,
+    {headers: { 'user-agent': userAgent }}
+  )
+  .then(({ data }) => console.log(data))
+  .catch(error => console.log(error));
+} 
+
+const postAnalytics = () => {
+  analyticsRequests.forEach((reqData) => {
+    analyticsRequestsQueue.then(() => {
+      sendAnalyticsRequest(reqData)
+    })
+  })
+
+  analyticsRequests = []; 
+}
+
+setInterval(() => {
+  internetAvailable().then(() => {
+    console.log('connected');
+    postAnalytics();
+  }).catch(() => console.log('no connection'))
+}, 20000);
+
+app.get(/pocket|converter|/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
